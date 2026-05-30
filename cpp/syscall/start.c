6840d42073154e2f92cd099b21146e82211d74a1
SrinivasaRimskyYamatov <rimsky.yamatov@gmail.com>
2026-05-30 16:41:13 +0900

    Refactor _start function to remove SYS_exit call

--- cpp/syscall/start.c
+++ cpp/syscall/start.c
@@ -1,10 +1,6 @@
 extern int main();
 extern long syscall(long, long, long, long, long, long, long);
 
-#define SYS_exit 60
-
-void _start()
-{
+void _start(){
     main();
-    syscall(SYS_exit, 0, 0, 0, 0, 0, 0);
 }
